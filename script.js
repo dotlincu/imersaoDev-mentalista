4ead254d9b2ee3daf38f019a15bcebcaa5164b82
Lincoln <lincoln.reboucas40@gmail.com>
2022-09-08 17:13:53 -0300

    one more commit

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 var numeroSecreto = parseInt(Math.random() * 11)
 
 function Chutar() {
-    var chute = parseInt(document.getElementById("valor").value)
-    console.log(chute)
+    var elementoResultado = document.getElementById("resultado");
+    var chute = parseInt(document.getElementById("valor").value);
 
-    if(chute == numeroSecreto){
-        console.log("Chute certo")
-    }
-
-    // document.getElementById("valor").innerHTML = ""
+    if(chute == numeroSecreto)
+        elementoResultado.innerHTML = "Acertou!";
+    else if(chute < 0 || chute > 10)
+        elementoResultado.innerHTML = "Você deve digitar um número entre 0 e 10!";
+    else if(chute > numeroSecreto)
+        elementoResultado.innerHTML = "Errou! O número secreto é menor!";
+    else if(chute < numeroSecreto)
+        elementoResultado.innerHTML = "Errou! O número secreto é maior!";
 }
